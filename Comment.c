b1cc5709ff67a8aebf2173ba2f89c236f4f3240e
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-15 22:17:12 +0100

    Update Comment.c

--- Comment.c
+++ Comment.c
@@ -11,38 +11,29 @@
  */ 
 int main(void)
 {
-  char input[MAX_INPUT_SIZE];
-
-  while (1) 
-  {
-    /* Print prompt */
-    printf("mysh> ");
-
-    /* Read input */
-    fgets(input, MAX_INPUT_SIZE, stdin);
-
-    /* Remove newline from input */
-    input[strlen(input) - 1] = '\0';
-
-    /* Skip empty lines */ 
-    if (strlen(input) == 0)
-    {
-      continue;
-    }
-
-    /* Check for comment */
-    if (input[0] == '#') 
-    {
-      continue; 
-    }
-
-    /* Add shell commands */
-
-  }
-
-  return (0);
+char input[MAX_INPUT_SIZE];
+while (1) 
+{
+/* Print prompt */
+printf("mysh> ");
+/* Read input */
+fgets(input, MAX_INPUT_SIZE, stdin);
+/* Remove newline from input */
+input[strlen(input) - 1] = '\0';
+/* Skip empty lines */ 
+if (strlen(input) == 0)
+{
+continue;
 }
-
+/* Check for comment */
+if (input[0] == '#') 
+{
+continue; 
+}
+/* Add shell commands */
+}
+return (0);
+}
 /**
  * print_prompt - Prints the shell prompt 
  *
