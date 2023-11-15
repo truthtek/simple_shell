ee2976d5b62f0bf99c4c8f2ddf7d27deffb99943
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-15 22:12:20 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -35,6 +35,12 @@ int start_shell(void);
 void prompt(void);
 
 void execute_command(char *command);
+char* readInput(char* input, int maxInputSize);
 
+int isComment(char* input);
+
+int executeCommand(char* input);
+
+int main();
 
 #endif /* SHELL_H */
