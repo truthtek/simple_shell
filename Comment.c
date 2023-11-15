dc2f29276a25cd208ed14d84b58746a88541b54c
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-15 22:11:32 +0100

    Update Comment.c

--- Comment.c
+++ Comment.c
@@ -1,34 +1,54 @@
+#include "shell.h
 #include <stdio.h>
 #include <string.h>
 
 #define MAX_INPUT_SIZE 1024
 
-int main() {
+/**
+ * main - Simple shell program with Betty style  
+ *
+ * Return: 0 on success
+ */ 
+int main(void)
+{
   char input[MAX_INPUT_SIZE];
-  
-  while (1) {
-    // Print prompt
+
+  while (1) 
+  {
+    /* Print prompt */
     printf("mysh> ");
-    
-    // Read input
+
+    /* Read input */
     fgets(input, MAX_INPUT_SIZE, stdin);
-    
-    // Remove newline from input
+
+    /* Remove newline from input */
     input[strlen(input) - 1] = '\0';
-    
-    // Skip empty lines
-    if (strlen(input) == 0) {
+
+    /* Skip empty lines */ 
+    if (strlen(input) == 0)
+    {
       continue;
     }
-    
-    // Check for comment
-    if (input[0] == '#') {
+
+    /* Check for comment */
+    if (input[0] == '#') 
+    {
       continue; 
     }
-    
-    // Add your shell commands here
-    
+
+    /* Add shell commands */
+
   }
-  
-  return 0;
+
+  return (0);
+}
+
+/**
+ * print_prompt - Prints the shell prompt 
+ *
+ * Return: Void
+ */
+void print_prompt(void)
+{
+  printf("mysh> ");
 }
