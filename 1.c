5d57ed3525b1eb24c8286216669013da64b92655
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-16 09:20:15 +0000

    Update 1.c

--- 1.c
+++ 1.c
@@ -9,32 +9,27 @@
  */
 int main(void)
 {
-    char input[MAX_INPUT_LENGTH];
-    pid_t pid;
-
-    while (1)
-    {
-        printf("#cisfun$ ");
-        if (fgets(input, sizeof(input), stdin) == NULL)
-            break;
-
-        input[strcspn(input, "\n")] = '\0';
-
-        if (strcmp(input, "exit") == 0)
-            break;
-
-        pid = fork();
-        if (pid == 0)
-        {
-            execlp(input, input, (char*)NULL);
-            printf("%s: command not found\n", input);
-            exit(0);
-        }
-        else
-        {
-            wait(NULL);
-        }
-    }
-
-    return (0);
+char input[MAX_INPUT_LENGTH];
+pid_t pid;
+while (1)
+{
+printf("#cisfun$ ");
+if (fgets(input, sizeof(input), stdin) == NULL)
+break;
+input[strcspn(input, "\n")] = '\0';
+if (strcmp(input, "exit") == 0)
+break;
+pid = fork();
+if (pid == 0)
+{
+execlp(input, input, (char*)NULL);
+printf("%s: command not found\n", input);
+exit(0);
+}
+else
+{
+wait(NULL);
+}
+}
+return (0);
 }
